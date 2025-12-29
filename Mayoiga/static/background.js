5b61690a45ebcda51d7d4e8576d467f94569bf04
root <root@DESKTOP-M9ET3IL.localdomain>
2025-12-29 14:41:54 +0000

    Working on the JavaScript

--- Mayoiga/static/background.js
+++ Mayoiga/static/background.js
@@ -1,12 +1,24 @@
-const element = document.getElementById('back_img');
-let position = 0;
+/* let position = 0;
 
 function animate() {
   position += 1;
-  element.style.backgroundPosition = `${position}px 0`;
+  document.body.style.backgroundPosition = `0 ${position}px`;
   requestAnimationFrame(animate);
 }
 
 window.onload = function () {
   animate();
-};
+}; */
+
+let pos = 0;
+const time = 1000;
+function backmove() {
+	document.body.style.backgroundPosition = `50% ${pos}%`;
+	if (pos < 50) {
+	pos += `50 / ${time}`;
+	requestAnimationFrame(backmove);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", backmove);
+requestAnimationFrame(backmove);
